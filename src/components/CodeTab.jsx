bb112384707aa828d206917d1a5619341d5672ee
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-08-09 18:54:40 +0200

    Add changing code feature

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,11 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { updateCode } from '../features/tabs';
 
 export default function CodeTab({ id, code }) {
-  const tabs = useSelector((state) => state.tabs);
+  const dispatch = useDispatch();
 
   return (
     <textarea
       value={code}
+      onChange={(e) => dispatch(updateCode({ id, value: e.target.value }))}
       spellCheck="false"
       className="bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none"
     ></textarea>
